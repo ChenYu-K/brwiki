1441422e543d0590ff58b24bad10766223766853
Yu Chen <71476156+ChenYu-K@users.noreply.github.com>
2022-05-21 12:15:37 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 yum install wget
 mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \
-   tar xvzf - --strip-components 1 -C ./pandoc
+wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \tar xvzf - --strip-components 1 -C ./pandoc
 export PATH="./pandoc/bin:$PATH"
 
 yarn run build

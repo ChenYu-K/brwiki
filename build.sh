a02f6159211f66c2bfde90ee187b5604227d34d1
Yu Chen <71476156+ChenYu-K@users.noreply.github.com>
2022-05-21 12:16:37 +0900

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-yum install wget
-mkdir pandoc
-wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | \tar xvzf - --strip-components 1 -C ./pandoc
-export PATH="./pandoc/bin:$PATH"
-
-yarn run build
